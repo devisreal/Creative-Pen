e3b1dd1e51b62dedd6536f1138009ab9ed67088d
Israel Akinoso <akinosoissy@gmail.com>
2022-11-28 12:46:14 +0100

    Complete footer design

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-   path('dashboard/', views.dashboard, name="dashboard"),
+   path('', views.dashboard, name="dashboard"),
    path('profile/', views.user_profile, name="profile"),
    path('edit-profile/', views.edit_profile, name="edit_profile"),
    path('delete-account', views.delete_account, name='delete_account')
