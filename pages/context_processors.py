262381495352d63fe8aedee184238ed32786b4f4
Israel Akinoso <akinosoissy@gmail.com>
2023-02-28 17:02:53 +0100

    feat: Home page trending topics completed

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -3,15 +3,16 @@ from account.models import User
 from django.db.models import Count
 import random
 def categories_processor(request):
-   footer_categories = PostCategory.objects.all()[:1]   
+   footer_categories = PostCategory.objects.all()[0:]   
    popular_categories = list(PostCategory.objects.annotate(posts_count=Count('post')).all())
-   random_categories = random.sample(popular_categories, 1)
+   total_category_count = PostCategory.objects.all().count()   
+   random_categories = random.sample(popular_categories, total_category_count)
    random_item = random.choice(popular_categories)
    author_requests = User.objects.filter(usersettings__request_author_access=True).count()
    context = {
       'footer_categories': footer_categories,
       'popular_categories': random_categories,
-      'author_requests': author_requests
+      'author_requests': author_requests,      
    }
    return context
 
